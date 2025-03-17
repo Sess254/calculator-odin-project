d85cafad6b5d7caf6dd6c97b0aa1f06225660c3b
sess254@fedora <wamaritemoses@gmail.com>
2025-03-17 12:49:01 +0300

    added functions that clear dom, does the calculations and shows the results

--- main.js
+++ main.js
@@ -4,20 +4,70 @@
 
 // append second number to the screen
 
-let currentNumber = "";
-let previousNumber = "";
-let currentOperand = "";
-
-
-
 const display = document.querySelector('.display');
 
-function appendToDisplay(number) {
+
+let currentNumber = "";
+let previousNumber = "";
+let currentOperator = "";
+let isResult = false;
+
+
+
+
+function appendNumberToDisplay(number) {
     currentNumber += number;
-    display.value = `${previousNumber}  ${currentOperand} ${currentNumber}`;
+    display.value = `${previousNumber} ${currentOperator} ${currentNumber}`;
 
 }
 
+function appendOperandToDisplay(operator) {
+    if (currentNumber === "") return;
+    if (previousNumber !== "") {
+        getCalculation();
+    }
+    currentOperator = operator;
+    previousNumber = currentNumber;
+    currentNumber = "";
+    display.value = `${previousNumber} ${currentOperator}`;
+}
+
 function getCalculation() {
-    console.log("calcultaing")
+    if (currentNumber === "" || previousNumber === "") return;
+    let result;
+    let prev = parseFloat(previousNumber);
+    let current = parseFloat(currentNumber);
+
+    switch (currentOperator) {
+        case '+': 
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case 'x':
+            result = prev * current;
+            break;
+        case '/': 
+            if (current === 0) {
+                alert('Cannot divide by zero!');
+                return;
+            }
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+
+    currentNumber = result.toString();
+    previousNumber = '';
+    currentOperator = '';
+    display.value = currentNumber;
+}
+
+function clearDisplay() {
+    currentNumber = '';
+    previousNumber = '';
+    currentOperator= '';
+    display.value = '';
 }
